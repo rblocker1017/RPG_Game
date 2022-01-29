806885dcb18753894d9e7ca8a6b5fcdb816d5de2
Ryan Blocker <ryanblocker85@gmail.com>
2022-01-28 17:46:43 -0800

    Fullscreen toggle, still buggy

--- src/engine/io/Window.java
+++ src/engine/io/Window.java
@@ -2,6 +2,9 @@ package engine.io;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.glfw.GLFWWindowSizeCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 
 public class Window {
     private int width, height;
@@ -10,12 +13,21 @@ public class Window {
     public int frames;
     public static long time;
     public Input input;
+    private float backgroundR, backgroundG, backgroundB;
+    private GLFWWindowSizeCallback sizeCallback;
+    private boolean isResized;
+    private boolean isFullScreen;
+    private int[] windowPosX = new int[1], windowPosY = new int[1];
+    private int minimizedWidth, minimizedHeight;
 
 
     public Window(int width, int height, String title){
         this.width = width;
         this.height = height;
         this.title = title;
+
+        minimizedWidth = width;
+        minimizedHeight = height;
     }
 
     public void create(){
@@ -24,7 +36,7 @@ public class Window {
             return;
         }
         input = new Input();
-        window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
+        window = GLFW.glfwCreateWindow(width, height, title, isFullScreen ? GLFW.glfwGetPrimaryMonitor() : 0 , 0);
 
         if(window == 0){
             System.err.println("ERROR: Window wasn't created.");
@@ -33,20 +45,46 @@ public class Window {
 
         GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
 
-        GLFW.glfwSetWindowPos(window, (videoMode.width() - width)/2, (videoMode.height() - height)/2);
+        windowPosX[0] = (videoMode.width() - width)/2;
+        windowPosY[0] = (videoMode.height() - height)/2;
+        GLFW.glfwSetWindowPos(window, windowPosX[0], windowPosY[0]);
+
+
         GLFW.glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
 
-        GLFW.glfwSetKeyCallback(window, input.getKeyboard());
-        GLFW.glfwSetCursorPosCallback(window, input.getMouseMove());
-        GLFW.glfwSetMouseButtonCallback(window, input.getMouseButtons());
-
+        createCallBacks();
         GLFW.glfwShowWindow(window);
         GLFW.glfwSwapInterval(1);
 
         time = System.currentTimeMillis();
     }
+    private void createCallBacks(){
+        sizeCallback = new GLFWWindowSizeCallback(){
 
+            @Override
+            public void invoke(long window, int w, int h) {
+                width = w;
+                height = h;
+                isResized = true;
+            }
+        };
+
+        GLFW.glfwSetKeyCallback(window, input.getKeyboard());
+        GLFW.glfwSetCursorPosCallback(window, input.getMouseMove());
+        GLFW.glfwSetMouseButtonCallback(window, input.getMouseButtons());
+        GLFW.glfwSetWindowSizeCallback(window,sizeCallback);
+
+    }
     public void update(){
+        if(isResized){
+            GL11.glViewport(0, 0, width, height);
+            isResized = false;
+        }
+        GL11.glClearColor(backgroundR, backgroundG, backgroundB, 1.0f);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
         GLFW.glfwPollEvents();
         frames++;
         if(System.currentTimeMillis() > time + 1000){
@@ -66,8 +104,71 @@ public class Window {
 
     public void destroy(){
         input.destroy();
+        GLFW.glfwTerminate();
         GLFW.glfwWindowShouldClose(window);
         GLFW.glfwDestroyCursor(window);
-        GLFW.glfwTerminate();
+    }
+
+    public void setBackgroundColor(float r, float g, float b){
+        backgroundR = r;
+        backgroundG = g;
+        backgroundB = b;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public long getWindow() {
+        return window;
+    }
+
+    public void setWindow(long window) {
+        this.window = window;
+    }
+
+    public boolean isFullScreen() {
+        return isFullScreen;
+    }
+
+    public void setFullScreen(boolean fullScreen) {
+        isFullScreen = fullScreen;
+        isResized = true;
+
+        if(isFullScreen){
+            GLFW.glfwGetWindowPos(window, windowPosX, windowPosY);
+
+            int tempWidth = width;
+            int tempHeight = height;
+
+            GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowMonitor(window, GLFW.glfwGetPrimaryMonitor(), 0, 0, videoMode.width(), videoMode.height(), 10);
+
+            width = tempWidth;
+            height = tempHeight;
+        }
+        else{
+            GLFW.glfwSetWindowMonitor(window, 0, windowPosX[0], windowPosY[0], width, height, 0);
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -14,23 +14,26 @@ public class Main implements Runnable{
         game.start();
     }
     public void init(){
-        System.out.println("Initializing Game!");
         window = new Window(WIDTH, HEIGHT, "RPG Game");
+        window.setBackgroundColor(0.0f,0.0f, 0.3f);
+
         window.create();
     }
     public void run(){
         init();
-        while(!window.shouldClose()){
+        while(!window.shouldClose() && !Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)){
             update();
             render();
-            if(Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE)) return;
         }
         window.destroy();
     }
     private void update(){
         window.update();
         if(Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)){
-            System.out.println("X: " + Input.getMouseX() + " Y: " + Input.getMouseY());
+
+        }
+        if(Input.isKeyDown(GLFW.GLFW_KEY_F11)) {
+            window.setFullScreen(!window.isFullScreen());
         }
 
     }
